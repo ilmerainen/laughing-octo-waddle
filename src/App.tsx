796d62fdedce1ce305232a9ea8b687c1faf44929
ilmerainen <rborukha@gmail.com>
2020-12-08 20:02:48 +0200

    fix: fix infinite scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,78 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
+import {useApolloClient} from "@apollo/client";
 
-import {INITIAL_POSTS_AMOUNT, INITIAL_POSTS_PAGE} from "./constants";
-import useIsPageBottom from "hooks/useIsPageBottom";
+import {INITIAL_POSTS_AMOUNT} from "./constants";
 import Post from "components/Post/Post";
 import {IPostData, IPostsData, IPostsVariables} from "interfaces";
+import {GET_ALL_POSTS} from "queries";
 import 'main.sass';
-import {useLazyQuery, useQuery} from "@apollo/client";
-import {GET_ALL_POSTS} from "./queries";
 
 function App() {
-    const [isBottom, setIsBottom] = useIsPageBottom();
-    const [postsPageNumber, setPostsPageNumber] = useState<number>(INITIAL_POSTS_PAGE);
-    const [postsData, setPostsData] = useState<{ totalCount: number | null, posts: IPostData[] }>({
+    const root = useRef<HTMLDivElement>(null);
+    const [postsData, setPostsData] = useState<{ totalCount: number | null, posts: IPostData[], loading: boolean }>({
         posts: [],
         totalCount: null,
-    });
-    const {loading, data} = useQuery<IPostsData, IPostsVariables>(GET_ALL_POSTS, {
-        variables: {
-            options: {
-                paginate: {
-                    page: postsPageNumber,
-                    limit: INITIAL_POSTS_AMOUNT,
-                },
-            },
-        },
+        loading: false,
     });
 
-    useEffect(function setPostsDataFromResponse() {
-        if (!data) {
+    const apolloClient = useApolloClient();
+
+    const getPosts = useCallback(() => {
+        console.log(postsData.posts.length / INITIAL_POSTS_AMOUNT + 1)
+        if (postsData.loading) {
             return;
         }
 
         setPostsData(postsData => ({
-                totalCount: data.posts.meta.totalCount,
-                posts: postsData.posts.concat(data.posts.data),
-            })
-        );
-    }, [data]);
+            ...postsData,
+            loading: true,
+        }));
 
-    useEffect(function handleScrollingToPageBottom() {
-        if (postsData.totalCount === postsData.posts.length || !isBottom || loading) {
-            return;
+        apolloClient.query<IPostsData, IPostsVariables>({
+            query: GET_ALL_POSTS,
+            variables: {
+                options: {
+                    paginate: {
+                        page: postsData.posts.length / INITIAL_POSTS_AMOUNT + 1,
+                        limit: INITIAL_POSTS_AMOUNT,
+                    },
+                },
+            },
+        }).then(results => {
+            setPostsData(postsData => {
+                return {
+                    ...postsData,
+                    posts: postsData.posts.concat(results.data.posts.data),
+                    totalCount: results.data.posts.meta.totalCount,
+                    loading: false,
+                };
+            });
+        })
+            .catch(test => console.log(test));
+    }, [apolloClient, postsData]);
+
+    const handleObserver = useCallback((entries: IntersectionObserverEntry[]) => {
+        const target = entries[0];
+
+        if (target.isIntersecting) {
+            if (postsData.totalCount && postsData.posts.length === postsData.totalCount) {
+                return;
+            }
+
+            getPosts();
         }
+    }, [getPosts, postsData]);
 
-        setIsBottom(false);
-        setPostsPageNumber(n => n + 1);
-    }, [loading, isBottom, setIsBottom, postsPageNumber, postsData]);
+    useEffect(() => {
+        const io = new IntersectionObserver(handleObserver, {
+            root: null,
+            threshold: 1,
+        });
+        const target = root.current!;
+        io.observe(target);
+
+        return () => io.unobserve(target);
+    }, [root, handleObserver]);
 
     return (
         <div className="container">{
@@ -53,7 +80,7 @@ function App() {
                 <Post key={post.id} title={post.title} content={post.body} user={post.user}/>
             ))
         }
-            {loading && 'Loading...'}
+            <div style={{visibility: postsData.loading ? 'visible' : 'hidden'}} ref={root}>Loading...</div>
         </div>
     );
 }
